c9045f3d86c092b00257e65d330a3c5a8e9d9351
Tamara Kohler <tamara.kohler.16@ucl.ac.uk>
2017-01-02 21:37:30 +0000

    Corrected command.py

--- greengraph/command.py
+++ greengraph/command.py
@@ -5,9 +5,9 @@ from matplotlib import pyplot as plt
 def process():
     parser = ArgumentParser(description = "Determine starting and ending locations")
     
-    parser.add_argument('start')
-    parser.add_argument('end')
-    parser.add_argument('steps')
+    parser.add_argument('--from', '-f')
+    parser.add_argument('--to', 't')
+    parser.add_argument('--steps', 's')
     
     mygraph = Greengraph(arguments.start,arguments.end)
     data = mygraph.green_between(arguments.steps)
